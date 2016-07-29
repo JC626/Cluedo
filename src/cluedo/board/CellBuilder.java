2912d96625d7eeda9a6bfbccd705cf2287a64e09
kxmo <kxmo@users.noreply.github.com>
2016-07-30 11:36:15 +1200

    X and Y were around the wrong way

--- src/cluedo/board/CellBuilder.java
+++ src/cluedo/board/CellBuilder.java
@@ -77,7 +77,7 @@ class CellBuilder
 					{ "SW00", "S000", "S000", "S000", "S000", "SE00", "0000", "ESWA", "0000", "0000", "SW00", "S000", "S000", "SE00", "0000", "0000", "ESW0", "0000", "SW00", "S000", "S000", "S000", "S000", "SE00" },
 			};
 
-		cells = new Cell[map.length][map[0].length];
+		cells = new Cell[map[0].length][map.length];
 
 		for (int row = 0; row < map.length; row++)
 		{
