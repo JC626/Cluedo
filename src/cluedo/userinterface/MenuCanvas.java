fc05a1d10cc01309bf0be023eb6d219edd5a3d77
kxmo <kxmo@users.noreply.github.com>
2016-08-06 15:06:43 +1200

    Cleaned up slightly; removed extra main

--- src/cluedo/userinterface/MenuCanvas.java
+++ src/cluedo/userinterface/MenuCanvas.java
@@ -7,32 +7,38 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class MenuCanvas extends JPanel{
+public class MenuCanvas extends JPanel
+{
 	private static final long serialVersionUID = 1L;
 	private boolean visible = true;
+
 	public MenuCanvas()
 	{
-		//Add components here
 		JTextArea title = new JTextArea("Cluedo");
 		JButton newGame = new JButton("New Game");
 		JButton quit = new JButton("Quit");
-		quit.addActionListener(e -> {
-			int answer = JOptionPane.showConfirmDialog(this, new JLabel("Do you want to quit?"),
-					"Do you want to quit?", JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE);
-		    if(answer == 0)
-		    {
-	            System.exit(0);
-		    }
-		});
+
+		quit.addActionListener(
+				e -> {
+					int answer = JOptionPane.showConfirmDialog(this, new JLabel("Do you want to quit?"), "Do you want to quit?",
+							JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+					if(answer == 0)
+					{
+						System.exit(0);
+					}
+				});
+
+		title.setEditable(false);
+
 		add(title);
-		
 		add(newGame);
 		add(quit);
 	}
 
-	
-	public boolean isVisible() {
+
+	public boolean isVisible()
+	{
 		return visible;
 	}
-	
+
 }
--- src/cluedo/userinterface/MenuFrame.java
+++ src/cluedo/userinterface/MenuFrame.java
@@ -4,23 +4,24 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class MenuFrame extends JFrame{
-	MenuCanvas canvas;
+public class MenuFrame extends JFrame
+{
 	public static final int WIDTH = 600;
 	public static final int HEIGHT = 600;
-	public MenuFrame(){
+	
+	MenuCanvas canvas;
+		
+	public MenuFrame()
+	{
 		super("Cluedo");
+		
 		canvas = new MenuCanvas();
 		setLayout(new BorderLayout());
-		add(canvas,BorderLayout.CENTER);
+		add(canvas, BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		pack();
 		setResizable(false);
 		setVisible(true);
-	}
-	public static void main(String[] args)
-	{
-		new MenuFrame();
 	}
 }
