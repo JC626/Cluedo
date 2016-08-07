c2ccd814f24759219aee885e82d3c19e80d77351
JC626 <janice.chin626@gmail.com>
2016-08-07 15:24:54 +1200

    Added board size prototype

--- src/cluedo/userinterface/BoardFrame.java
+++ src/cluedo/userinterface/BoardFrame.java
@@ -1,10 +1,29 @@
 package cluedo.userinterface;
+
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class BoardFrame extends JFrame{
 	private static final long serialVersionUID = 1L;
+	static final int WIDTH = 1000;
+	static final int HEIGHT = 1000;
+	BoardCanvas board;
 	public BoardFrame()
 	{
-		
+		this.setTitle("Cluedo Game");
+		this.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+		board = new BoardCanvas();
+		JPanel bottom = new JPanel();
+		this.add(board);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+		setResizable(false);
+	}
+	public static void main(String[] args){
+		new BoardFrame();
 	}
 }
