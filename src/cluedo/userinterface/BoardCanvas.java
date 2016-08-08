462d174155dce09985b3a7a1d29f133ec9049aeb
kxmo <kxmo@users.noreply.github.com>
2016-08-08 14:14:04 +1200

    General cleaning, documentation, and removal of hard coded values

--- src/cluedo/userinterface/BoardCanvas.java
+++ src/cluedo/userinterface/BoardCanvas.java
@@ -1,36 +1,43 @@
 package cluedo.userinterface;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.LayoutManager;
-import java.awt.Rectangle;
-import java.awt.image.ImageObserver;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JPanel;
-
 import cluedo.board.Board;
 
-public class BoardCanvas extends JPanel {
+public class BoardCanvas extends JPanel
+{
 	private static final long serialVersionUID = 1L;
-	static final int HEIGHT = 800;
-	public static final int cellWidth = BoardFrame.WIDTH/24;
-	public static final int cellHeight = HEIGHT/25;
+	
+	static final int HEIGHT = 800; // FIXME, why is this here? Should it be with BoardFrame.WIDTH, or should BoardFrame.WIDTH be here?
+	
+	public static final int cellWidth = BoardFrame.WIDTH / Board.WIDTH;
+	public static final int cellHeight = HEIGHT / Board.HEIGHT;
+
 	private Image[][] boardImages;
-	
-	
-	public BoardCanvas(Image[][] boardImages) {
-		super();
+		
+	public BoardCanvas(Image[][] boardImages)
+	{
+		if (boardImages == null)
+		{
+			throw new IllegalArgumentException("Arguments may not be null");
+		}
+		
 		this.boardImages = boardImages;
 	}
 
-	protected void paintComponent(Graphics g){
-		for(int x = 0; x < Board.WIDTH; x++){
-			for(int y = 0; y < Board.HEIGHT; y++){
+	/**
+	 * Draw boardImages as provided in the constructor onto Graphics g.
+	 * If boardImages contains null images, they are not drawn.
+	 */
+	protected void paintComponent(Graphics g)
+	{
+		for (int x = 0; x < Board.WIDTH; x++)
+		{
+			for (int y = 0; y < Board.HEIGHT; y++)
+			{
 				Image image = boardImages[x][y];
-				g.drawImage(image, x*cellWidth, y*cellHeight, null);
+				g.drawImage(image, x * cellWidth, y * cellHeight, null);
 			}
 
 		}
