bbcfa9b2dd1da01cb42ff2613a518685e8cbaabd
kxmo <kxmo@users.noreply.github.com>
2016-08-08 21:42:47 +1200

    Style and readability changes. Questions on names and values

--- src/cluedo/userinterface/DiceCanvas.java
+++ src/cluedo/userinterface/DiceCanvas.java
@@ -16,35 +16,43 @@ import javax.swing.JPanel;
  * for the player to know how many
  * moves they have
  */
-public class DiceCanvas extends JPanel {
+public class DiceCanvas extends JPanel
+{
 	private static final long serialVersionUID = 1L;
-	private int WIDTH = (int)(BoardFrame.WIDTH*0.3);
-	private int HEIGHT =  BoardFrame.HEIGHT - BoardCanvas.HEIGHT;
+	
+	private int WIDTH = (int)(BoardFrame.WIDTH * 0.3); // FIXME why 0.3?
+	private int HEIGHT =  BoardFrame.HEIGHT - BoardCanvas.HEIGHT; // TODO is this value not stored elsewhere? Seems like it should be in the bottom panel frame
+	
 	private int IMAGE_SIZE = 60;
+	
 	private JLabel dice1;
 	private JLabel dice2;
 	
-	public DiceCanvas(Image img1, Image img2) {
-		super();
-		this.setLayout(new GridLayout(0, 2,10,0));
-		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
-		Image d1 = img1.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
-		Image d2 = img2.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+	public DiceCanvas(Image leftDie, Image rightDie) // FIXME Is my assumption on left and right correct? Was img1 img2 
+	{
+		Image d1 = leftDie.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+		Image d2 = rightDie.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+		
 		dice1 = new JLabel(new ImageIcon(d1));
 		dice2  = new JLabel(new ImageIcon(d2));
+		
 		this.add(dice1);
 		this.add(dice2);
+		
+		this.setLayout(new GridLayout(0, 2, 10, 0));
+		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
 	}
 	/**
 	 * Changes the dice displayed when the player
-	 * starts their turn
-	 * @param img1
-	 * @param img2
+	 * starts their turn.
+	 * @param leftDie
+	 * @param rightDie
 	 */
-	public void changeDice(Image img1, Image img2)
+	public void changeDice(Image leftDie, Image rightDie) // TODO same as constructor FIX ME
 	{
-		Image d1 = img1.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
-		Image d2 = img2.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+		Image d1 = leftDie.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+		Image d2 = rightDie.getScaledInstance(IMAGE_SIZE, IMAGE_SIZE, Image.SCALE_DEFAULT);
+		
 		dice1.setIcon(new ImageIcon(d1));
 		dice2.setIcon(new ImageIcon(d2));
 	}
