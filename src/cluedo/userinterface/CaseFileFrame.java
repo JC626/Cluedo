6e80a0e3ab62a5b85d176ccae043a5b4074aead4
kxmo <kxmo@users.noreply.github.com>
2016-08-18 16:00:19 +1200

    Center alligned OK button. Added documentation. Refactored.

--- src/cluedo/userinterface/CaseFileFrame.java
+++ src/cluedo/userinterface/CaseFileFrame.java
@@ -1,4 +1,5 @@
 package cluedo.userinterface;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
@@ -21,91 +22,104 @@ import javax.swing.table.TableModel;
  * suspicion.
  * A " " represents that the card may still be part of the murder
  */
-public class CaseFileFrame extends JFrame{
+public class CaseFileFrame extends JFrame
+{
 	private static final int MIN_WIDTH = 600;
 	private static final int MIN_HEIGHT = 600;
 	private static final float BUTTON_FONT_SIZE = 20;
 	private static final int VERTICAL_GAP = 30;
 
 	//TODO Should pass these into the constructor?
-	 private String[] suspectHeader = new String[]{
-			 "Suspects","\t"
-	 };
-	 private String[] weaponHeader = new String[]{
-			 "Weapons","\t"
-	 };
-	 private String[] roomHeader = new String[]{
-			 "Rooms","\t"
-	 };
+	private String[] suspectHeader = {"Suspects", "\t"};
+	private String[] weaponHeader = {"Weapons", "\t"};
+	private String[] roomHeader = {"Rooms", "\t"};
 
+	/**
+	 * Create the tables using the provided columns headers
+	 * Each parameter represents a single table to be created.
+	 * @param suspectRows
+	 * @param weaponRows
+	 * @param roomRows
+	 */
 	public CaseFileFrame(String[][] suspectRows, String[][] weaponRows, String[][] roomRows) 
 	{
 		this.setTitle("Your Casefile");
-		this.setMinimumSize(new Dimension(MIN_WIDTH,MIN_HEIGHT));
+		this.setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
 
+		JPanel panel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		JLabel title = new JLabel("Your Casefile");
+		
 		JScrollPane suspects = createTable(suspectHeader,suspectRows);
 		JScrollPane weapons = createTable(weaponHeader,weaponRows);
 		JScrollPane rooms = createTable(roomHeader,roomRows);
-
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		JButton ok = new JButton("OK");
-		JLabel title = new JLabel("Your Casefile");
 		
-		ok.addActionListener(a -> {
+		JButton affirmativeButton = new JButton("OK");
+		affirmativeButton.addActionListener(a -> {
 			this.dispose();
 		});
-		
-		GraphicalUserInterface.setFontSize(ok, BUTTON_FONT_SIZE);
+
+		GraphicalUserInterface.setFontSize(affirmativeButton, BUTTON_FONT_SIZE);
 		GraphicalUserInterface.setFontSize(title, BUTTON_FONT_SIZE);
-		
+
 		//Align everything to the left
 		title.setAlignmentX(LEFT_ALIGNMENT);
 		suspects.setAlignmentX(LEFT_ALIGNMENT);
 		weapons.setAlignmentX(LEFT_ALIGNMENT);
 		rooms.setAlignmentX(LEFT_ALIGNMENT);
-		ok.setAlignmentX(LEFT_ALIGNMENT);
 		
+		// Line up our button appropriately
+		buttonPanel.setAlignmentX(LEFT_ALIGNMENT);
+		affirmativeButton.setAlignmentX(CENTER_ALIGNMENT);
+
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		panel.add(title);
 		panel.add(Box.createVerticalStrut(VERTICAL_GAP)); 
 		panel.add(suspects);
 		panel.add(weapons);
 		panel.add(rooms);
 		panel.add(Box.createVerticalStrut(VERTICAL_GAP));
-		panel.add(ok);
+		
+		panel.add(buttonPanel);
+		buttonPanel.add(affirmativeButton);
+		
 		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 50, 20));
 		this.add(panel);
+		
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		pack();
 		setVisible(true);
 		setResizable(false);
 	}
-		
+
 	/**
 	 * Create the tables using the provided columns headers 
 	 * and the values in each row
-	 * @param columnHeaders
-	 * @param rowItems
-	 * @return A table
+	 * @param columnHeaders The string values in each header.
+	 * @param rowItems The items in each row.
+	 * @return An table that cannot be changed by the user. 
 	 */
 	private JScrollPane createTable(String[] columnHeaders, String[][] rowItems)
 	{
 		TableModel model = new DefaultTableModel(rowItems, columnHeaders)
-		  {
-		    public boolean isCellEditable(int row, int column)
-		    {
-		      return false;//This causes all cells to be not editable
-		    }
-		  };
+		{
+			public boolean isCellEditable(int row, int column)
+			{
+				return false; // This causes all cells to be not editable
+			}
+		};
+		
 		JTable table = new JTable(model);
+		JScrollPane scrollPane = new JScrollPane(table);
+		int height = (int)(table.getRowHeight() * (table.getRowCount() + 1.5)) - 1;
+		
 		table.getTableHeader().setReorderingAllowed(false);
 		table.setFillsViewportHeight(true);
 		table.setCellSelectionEnabled(false); 
-		int height = (int)(table.getRowHeight() * (table.getRowCount() + 1.5)) - 1;
-		//So headers are attached to the table
-		JScrollPane scrollPane = new JScrollPane(table);
+		
+		// Ensure headers are attached to the table
 		scrollPane.setEnabled(false);
-		scrollPane.setPreferredSize(new Dimension(MIN_WIDTH,height));
+		scrollPane.setPreferredSize(new Dimension(MIN_WIDTH, height));
 		return scrollPane;
 	}
 
